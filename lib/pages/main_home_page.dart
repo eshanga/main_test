031a2e33bef5e60cec1c35be65203a5db2d3c52d
eshanga <98296029+eshanga@users.noreply.github.com>
2023-03-29 12:49:32 +0530

    Updated with news

--- lib/pages/main_home_page.dart
+++ lib/pages/main_home_page.dart
@@ -2,6 +2,9 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import 'package:velocity_x/velocity_x.dart';
+import '../components/customListTile.dart';
+import '../model/article_model.dart';
+import '../services/api_service.dart';
 import 'app_drawer.dart';
 import 'expense_tracker/home_expense.dart';
 import 'screen2/screen2.dart';
@@ -18,6 +21,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  ApiService client = ApiService();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -45,54 +49,83 @@ class _HomeScreenState extends State<HomeScreen> {
       drawer: AppDrawer(
         email: '',
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ElevatedButton(
-              child: Text('Expense Tracker'),
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => HomeExpense()));
-              },
+      body: Column(
+        children: [
+          FutureBuilder(
+            future: client.getArticle(),
+            builder:
+                (BuildContext context, AsyncSnapshot<List<Article>> snapshot) {
+              //let's check if we got a response or not
+              if (snapshot.hasData) {
+                //Now let's make a list of articles
+                List<Article>? articles = snapshot.data;
+                return SizedBox(
+                  height: 350.0,
+                  child: ListView.builder(
+                    shrinkWrap: true,
+                    //Now let's create our custom List tile
+                    scrollDirection: Axis.horizontal,
+                    itemCount: 5,
+                    itemBuilder: (context, index) =>
+                        customListTile(articles![index], context),
+                  ),
+                );
+              }
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            },
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                ElevatedButton(
+                  child: Text('Expense Tracker'),
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => HomeExpense()));
+                  },
+                ),
+                ElevatedButton(
+                  child: Text('Screen 2'),
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => Screen2()));
+                  },
+                ),
+                ElevatedButton(
+                  child: Text('Screen 3'),
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => Screen3()));
+                  },
+                ),
+                ElevatedButton(
+                  child: Text('Screen 4'),
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => Screen4()));
+                  },
+                ),
+                ElevatedButton(
+                  child: Text('Screen 5'),
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => Screen5()));
+                  },
+                ),
+                ElevatedButton(
+                  child: Text('Screen 6'),
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => Screen6()));
+                  },
+                ),
+              ],
             ),
-            ElevatedButton(
-              child: Text('Screen 2'),
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => Screen2()));
-              },
-            ),
-            ElevatedButton(
-              child: Text('Screen 3'),
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => Screen3()));
-              },
-            ),
-            ElevatedButton(
-              child: Text('Screen 4'),
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => Screen4()));
-              },
-            ),
-            ElevatedButton(
-              child: Text('Screen 5'),
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => Screen5()));
-              },
-            ),
-            ElevatedButton(
-              child: Text('Screen 6'),
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => Screen6()));
-              },
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
